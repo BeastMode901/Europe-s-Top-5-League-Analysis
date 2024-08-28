80df09dde0f4f100ecc722dd49b6dab58104c767
Collins M <collinsmensah4478@outlook.com>
2024-08-28 16:52:37 -0400

    Analysis Questions

---
+++ Analysis Questions.sql
@@ -0,0 +1,22 @@
+
+--Analysis Questions--
+
+/*
+1. What is the Distribution of Players among the Leagues?
+2. How does the distribution of players vary by continent?
+3. How are players from different continents distributed across the leagues?
+4. How does the market value of players compare across the 5 leagues?
+5. How does the market value of players compare across continents?
+6. How does the average market value of players vary across different age groups?
+7. How do teams within the 5 leagues rank based on their lowest, highest, average, and total market values?
+8. What is the ranking of forwards based on their goal-to-game ratio?
+9. What is the ranking of midfielders based on their assists-to-game ratio?
+10. What is the ranking of defenders based on their cards-to-game- ratio?
+11. Which players have achieved over 100 games, 100 goals, and 100 assists?
+12. How many players have never received a red card in their playing career?
+13. How are teams ranked based on the total number of career goals scored, assists made, and disciplinary cards received by their players?
+14. How are the leagues ranked according to the total number of career goals scored, assists made, and disciplinary cards received by their players?
+15. How do the average age, youngest, and oldest players vary across the leagues?
+16. What are the rankings of players within their teams and all players based on their years of professional experience?
+17. Which players have been playing for their current club for at least 5 years?
+*/
