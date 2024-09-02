db92c10f2dd26f5722aec43dd8d0e346b5ae3827
Collins M <collinsmensah4478@outlook.com>
2024-09-02 11:56:41 -0400

    Update Data Analysis Questions.sql

--- Data Analysis Questions.sql
+++ Data Analysis Questions.sql
@@ -5,7 +5,7 @@
 1. What is the distribution of players across the top 5 football leagues?
 2. How does player distribution differ by continent?
 3. Which country has the highest representation of players in this dataset?
-4. How does the market value of players vary among the top leagues?
+4. How does the market value of players vary among the leagues?
 5. How are teams ranked based on their players' average and total market values?
 6. How does the average market value of players differ across various age groups?
 7. What are the player rankings based on the number of years they've played professionally?
