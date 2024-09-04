fc979246dfecbc78af6be7d3e660d7841d6a18ea
Collins M <collinsmensah4478@outlook.com>
2024-09-04 15:59:23 -0400

    Add files via upload

---
+++ Project Analysis Questions.sql
@@ -0,0 +1,21 @@
+/*
+
+Questions Explored:
+
+1. What is the distribution of players across the 5 leagues?
+2. How does player distribution differ by continent?
+3. Which country has the highest representation of players in this dataset?
+4. How does the market value of players vary among the leagues?
+5. How are teams ranked based on their players' average and total market values?
+6. How does the average market value of players differ across various age groups?
+7. What are the player rankings based on the number of years they've played professionally?
+8. What is the average length of a professional playing career by position?
+9. Who are the top 10 players with the most career goals?
+10. Who are the top 20 players with the most career assists?
+11. Who are the top 10 players with the most cards accumulated?
+12. How many players with over 150 games have never received a red card in their career?
+13. Which players have been with their club for at least 5 years?
+14. Who are the top 5 most expensive players?
+
+*/
+
